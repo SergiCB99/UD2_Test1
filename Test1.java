f0524b05e3a5757ef277558766878179a0037d01
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-07-06 12:38:22 +0200

    Create Test1.java

---
+++ Test1.java
@@ -0,0 +1,13 @@
+package C1.UD2;
+
+public class Test1{
+	
+	public static void main(String[] args) {
+		
+		//System.out.println("Hola");	
+		System.out.println("Adios");
+		System.out.println("Fin del Programa");
+	
+	}
+
+}
